9777400bec0ca560ea50ab3f6b7b9aba9fa8c9a4
Ge Shi <utashih@gmail.com>
2020-10-22 13:00:45 -0700

    Start a new test suite on memory safety

---
+++ src/mwcas/mem_safety_tests.cc
@@ -0,0 +1,171 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+#include <stdlib.h>
+
+#include <atomic>
+
+#include "common/allocator_internal.h"
+#include "environment/environment_linux.h"
+#include "include/allocator.h"
+#include "include/environment.h"
+#include "include/pmwcas.h"
+#include "include/status.h"
+#include "mwcas/mwcas.h"
+#include "util/auto_ptr.h"
+#include "util/random_number_generator.h"
+
+namespace pmwcas {
+
+const uint32_t kDescriptorPoolSize = 0x400;
+const uint32_t kTestArraySize = 0x80;
+const uint32_t kWordsToUpdate = 4;
+
+struct AllocTestLinkedListNode {
+  uint64_t key;
+  nv_ptr<AllocTestLinkedListNode> next;
+};
+
+#ifdef PMDK
+struct RootObj {
+  PMEMoid pool;
+  PMEMoid array;
+};
+
+class PMwCASMemorySafetyTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    auto thread_count = Environment::Get()->GetCoreCount();
+    allocator_ = reinterpret_cast<PMDKAllocator *>(Allocator::Get());
+    auto root_obj = (RootObj *)allocator_->GetRoot(sizeof(RootObj));
+
+    pmemobj_zalloc(allocator_->GetPool(), &root_obj->pool,
+                   sizeof(pmwcas::DescriptorPool), TOID_TYPE_NUM(char));
+    new (pmemobj_direct(root_obj->pool))
+        pmwcas::DescriptorPool(kDescriptorPoolSize, thread_count);
+
+    pmemobj_zalloc(allocator_->GetPool(), &root_obj->array,
+                   sizeof(nv_ptr<AllocTestLinkedListNode>) * kTestArraySize,
+                   TOID_TYPE_NUM(char));
+
+    pool_ = (pmwcas::DescriptorPool *)pmemobj_direct(root_obj->pool);
+    array_ = (nv_ptr<AllocTestLinkedListNode> *)pmemobj_direct(root_obj->array);
+
+    {
+      PMEMobjpool *pop = allocator_->GetPool();
+      TOID(char) iter;
+      POBJ_FOREACH_TYPE(pop, iter) {
+        void *addr = pmemobj_direct(iter.oid);
+        base_allocations_.insert(addr);
+      }
+    }
+  }
+
+  PMDKAllocator *allocator_;
+  pmwcas::DescriptorPool *pool_;
+  nv_ptr<AllocTestLinkedListNode> *array_;
+
+  std::set<void *> base_allocations_;
+
+  bool isNewAllocation(void *addr) {
+    return base_allocations_.find(addr) == base_allocations_.end();
+  }
+};
+
+TEST_F(PMwCASMemorySafetyTest, SingleThreadSuccess) {
+  RandomNumberGenerator rng(rand(), 0, kTestArraySize);
+
+  nv_ptr<AllocTestLinkedListNode> *addresses[kWordsToUpdate];
+  nv_ptr<AllocTestLinkedListNode> values[kWordsToUpdate];
+
+  for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+    addresses[i] = nullptr;
+    values[i] = nullptr;
+  }
+
+  pool_->GetEpoch()->Protect();
+
+  for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+  retry:
+    uint64_t idx = rng.Generate();
+    for (uint32_t j = 0; j < i; ++j) {
+      if (addresses[j] == &array_[idx]) {
+        goto retry;
+      }
+    }
+
+    addresses[i] = &array_[idx];
+    values[i] =
+        reinterpret_cast<pmwcas::MwcTargetField<uint64_t> *>(&array_[idx])
+            ->GetValueProtected();
+    EXPECT_EQ(values[i], nullptr);
+  }
+
+  auto descriptor = pool_->AllocateDescriptor();
+  EXPECT_NE(nullptr, descriptor.GetRaw());
+
+  for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+    auto idx = descriptor.ReserveAndAddEntry((uint64_t *)addresses[i],
+                                             (uint64_t)values[i],
+                                             Descriptor::kRecycleAlways);
+    uint64_t *ptr = descriptor.GetNewValuePtr(idx);
+    allocator_->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode));
+  }
+
+  EXPECT_TRUE(descriptor.MwCAS());
+
+  std::set<void *> allocated;
+  for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+    AllocTestLinkedListNode *addr =
+        static_cast<nv_ptr<AllocTestLinkedListNode>>(
+            reinterpret_cast<pmwcas::MwcTargetField<uint64_t> *>(addresses[i])
+                ->GetValueProtected());
+    EXPECT_EQ(allocated.count(addr), 0);
+    allocated.insert(addr);
+  }
+
+  pool_->GetEpoch()->Unprotect();
+
+  std::set<void *> expected;
+  {
+    PMEMobjpool *pop = allocator_->GetPool();
+    TOID(char) iter;
+    POBJ_FOREACH_TYPE(pop, iter) {
+      void *addr = pmemobj_direct(iter.oid);
+      if (isNewAllocation(addr)) {
+        expected.insert(addr);
+      }
+    }
+  }
+
+  ASSERT_EQ(allocated.size(), expected.size());
+
+  for (auto i = allocated.begin(), j = expected.begin();
+       i != allocated.end() && j != allocated.end(); ++i, ++j) {
+    EXPECT_EQ(*i, *j);
+  }
+}
+
+#endif
+}  // namespace pmwcas
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  ::testing::InitGoogleTest(&argc, argv);
+  FLAGS_alsologtostderr = 1;
+
+#ifdef PMDK
+  pmwcas::InitLibrary(pmwcas::PMDKAllocator::Create(
+                          "mwcas_mem_safety_test_pool", "mwcas_alloc_layout",
+                          static_cast<uint64_t>(1024) * 1024 * 1204 * 1),
+                      pmwcas::PMDKAllocator::Destroy,
+                      pmwcas::LinuxEnvironment::Create,
+                      pmwcas::LinuxEnvironment::Destroy);
+#else
+  pmwcas::InitLibrary(
+      pmwcas::DefaultAllocator::Create, pmwcas::DefaultAllocator::Destroy,
+      pmwcas::LinuxEnvironment::Create, pmwcas::LinuxEnvironment::Destroy);
+#endif
+  return RUN_ALL_TESTS();
+}
