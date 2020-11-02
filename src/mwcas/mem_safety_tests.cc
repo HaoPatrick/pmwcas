1a18759785ad53f34b5ed90e3c6ec5b5632c18f8
Ge Shi <utashih@gmail.com>
2020-11-01 22:35:00 -0800

    Add a memory safety test with recovery

--- src/mwcas/mem_safety_tests.cc
+++ src/mwcas/mem_safety_tests.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+#include <fcntl.h>
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
@@ -21,10 +22,22 @@ namespace pmwcas {
 const uint32_t kDescriptorPoolSize = 0x400;
 const uint32_t kTestArraySize = 0x80;
 const uint32_t kWordsToUpdate = 4;
+const uint32_t kNumThreads = 4;
+const char *kSemaphoreName = "/mwcas-mem-safety-recovery-test-sem";
+
+static bool contains(const std::set<void *> &allocations, void *addr) {
+  return allocations.find(addr) != allocations.end();
+}
+
+static void insert(std::set<void *> &allocations, void *addr) {
+  EXPECT_TRUE(!contains(allocations, addr));
+  allocations.insert(addr);
+}
 
 struct AllocTestLinkedListNode {
-  uint64_t key;
   nv_ptr<AllocTestLinkedListNode> next;
+  uint64_t key;
+  uint64_t payload;
 };
 
 #ifdef PMDK
@@ -59,7 +72,7 @@ class PMwCASMemorySafetyTest : public ::testing::Test {
       TOID(char) iter;
       POBJ_FOREACH_TYPE(pop, iter) {
         void *addr = pmemobj_direct(iter.oid);
-        base_allocations_.insert(addr);
+        insert(base_allocations_, addr);
       }
     }
 
@@ -67,7 +80,7 @@ class PMwCASMemorySafetyTest : public ::testing::Test {
     for (uint32_t i = 0; i < kTestArraySize; ++i) {
       auto ptr = reinterpret_cast<uint64_t *>(&array2_[i]);
       allocator_->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode), false);
-      new (array2_[i]) AllocTestLinkedListNode{i, nullptr};
+      new (array2_[i]) AllocTestLinkedListNode{nullptr, i, 0};
     }
     // leave array1_ empty (i.e. with nullptr)
 
@@ -77,7 +90,7 @@ class PMwCASMemorySafetyTest : public ::testing::Test {
       POBJ_FOREACH_TYPE(pop, iter) {
         void *addr = pmemobj_direct(iter.oid);
         if (!contains(base_allocations_, addr)) {
-          user_allocations_.insert(addr);
+          insert(user_allocations_, addr);
         }
       }
     }
@@ -90,10 +103,6 @@ class PMwCASMemorySafetyTest : public ::testing::Test {
 
   std::set<void *> base_allocations_;
   std::set<void *> user_allocations_;
-
-  bool contains(std::set<void *> &allocations, void *addr) {
-    return allocations.find(addr) != allocations.end();
-  }
 };
 
 TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationSuccess) {
@@ -146,7 +155,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationSuccess) {
         reinterpret_cast<pmwcas::MwcTargetField<uint64_t> *>(addresses[i])
             ->GetValueProtected());
     EXPECT_FALSE(contains(allocated, addr));
-    allocated.insert(addr);
+    insert(allocated, addr);
   }
 
   pool_->GetEpoch()->Unprotect();
@@ -159,7 +168,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationSuccess) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr) &&
           !contains(user_allocations_, addr)) {
-        new_allocations.insert(addr);
+        insert(new_allocations, addr);
       }
     }
   }
@@ -244,7 +253,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationFailure) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr) &&
           !contains(user_allocations_, addr)) {
-        new_allocations.insert(addr);
+        insert(new_allocations, addr);
       }
     }
   }
@@ -297,7 +306,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationLeak) {
     uint64_t *ptr = descriptor.GetNewValuePtr(idx);
     // Incorrect use of AllocateOffset(). This will leak memory persistently.
     allocator_->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode), false);
-    allocated.insert(nv_ptr<AllocTestLinkedListNode>(*ptr));
+    insert(allocated, nv_ptr<AllocTestLinkedListNode>(*ptr));
   }
 
   EXPECT_EQ(allocated.size(), kWordsToUpdate);
@@ -330,7 +339,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadAllocationLeak) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr) &&
           !contains(user_allocations_, addr)) {
-        new_allocations.insert(addr);
+        insert(new_allocations, addr);
       }
     }
   }
@@ -385,7 +394,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadDeallocationSuccess) {
     auto idx =
         descriptor.AddEntry((uint64_t *)addresses[i], (uint64_t)values[i], 0ull,
                             Descriptor::kRecycleOldOnSuccess);
-    deallocated.insert((pmwcas::AllocTestLinkedListNode *)values[i]);
+    insert(deallocated, (pmwcas::AllocTestLinkedListNode *)values[i]);
   }
 
   EXPECT_EQ(deallocated.size(), kWordsToUpdate);
@@ -417,7 +426,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadDeallocationSuccess) {
     POBJ_FOREACH_TYPE(pop, iter) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr)) {
-        new_user_allocations.insert(addr);
+        insert(new_user_allocations, addr);
       }
     }
   }
@@ -477,7 +486,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadDeallocationFailure) {
     auto idx =
         descriptor.AddEntry((uint64_t *)addresses[i], (uint64_t)values[i], 0ull,
                             Descriptor::kRecycleOldOnSuccess);
-    deallocated.insert((pmwcas::AllocTestLinkedListNode *)values[i]);
+    insert(deallocated, (pmwcas::AllocTestLinkedListNode *)values[i]);
   }
 
   EXPECT_EQ(deallocated.size(), kWordsToUpdate);
@@ -510,7 +519,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadDeallocationFailure) {
     POBJ_FOREACH_TYPE(pop, iter) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr)) {
-        new_user_allocations.insert(addr);
+        insert(new_user_allocations, addr);
       }
     }
   }
@@ -593,7 +602,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadSwapNoAlloc) {
     POBJ_FOREACH_TYPE(pop, iter) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr)) {
-        new_user_allocations.insert(addr);
+        insert(new_user_allocations, addr);
       }
     }
   }
@@ -647,7 +656,8 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadRealloc) {
     allocator_->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode));
     AllocTestLinkedListNode *node =
         nv_ptr<AllocTestLinkedListNode>(descriptor.GetNewValue(entry));
-    new (node) AllocTestLinkedListNode{values->key, values->next};
+    new (node)
+        AllocTestLinkedListNode{values->next, values->key, values->payload};
 
     EXPECT_TRUE(descriptor.MwCAS());
 
@@ -670,7 +680,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadRealloc) {
     POBJ_FOREACH_TYPE(pop, iter) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr)) {
-        new_user_allocations.insert(addr);
+        insert(new_user_allocations, addr);
       }
     }
   }
@@ -735,7 +745,8 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadSwapRealloc) {
     allocator_->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode));
     AllocTestLinkedListNode *node =
         nv_ptr<AllocTestLinkedListNode>(descriptor.GetNewValue(entry));
-    new (node) AllocTestLinkedListNode{values[j]->key, values[j]->next};
+    new (node) AllocTestLinkedListNode{values[j]->next, values[j]->key,
+                                       values[j]->payload};
 
     EXPECT_TRUE(descriptor.MwCAS());
 
@@ -758,7 +769,7 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadSwapRealloc) {
     POBJ_FOREACH_TYPE(pop, iter) {
       void *addr = pmemobj_direct(iter.oid);
       if (!contains(base_allocations_, addr)) {
-        new_user_allocations.insert(addr);
+        insert(new_user_allocations, addr);
       }
     }
   }
@@ -779,6 +790,270 @@ TEST_F(PMwCASMemorySafetyTest, SingleThreadSwapRealloc) {
   Thread::ClearRegistry(true);
 }
 
+void child_process_work(sem_t *sem, std::set<void *> &base_allocations) {
+  static const size_t kOperationPerProtect = 100;
+  static const size_t kInsertPercentage = 75;
+
+  auto thread_count = Environment::Get()->GetCoreCount();
+  auto allocator = reinterpret_cast<PMDKAllocator *>(Allocator::Get());
+  auto root_obj = (RootObj *)allocator->GetRoot(sizeof(RootObj));
+
+  pmemobj_zalloc(allocator->GetPool(), &root_obj->pool,
+                 sizeof(pmwcas::DescriptorPool), TOID_TYPE_NUM(char));
+  new (pmemobj_direct(root_obj->pool))
+      pmwcas::DescriptorPool(kDescriptorPoolSize, thread_count);
+
+  pmemobj_zalloc(allocator->GetPool(), &root_obj->array,
+                 sizeof(nv_ptr<AllocTestLinkedListNode>) * kTestArraySize,
+                 TOID_TYPE_NUM(char));
+
+  auto pool = (pmwcas::DescriptorPool *)pmemobj_direct(root_obj->pool);
+  auto array =
+      (nv_ptr<AllocTestLinkedListNode> *)pmemobj_direct(root_obj->array);
+
+  // prefill array with some nodes
+  for (uint32_t i = 0; i < kTestArraySize; ++i) {
+    auto ptr = reinterpret_cast<uint64_t *>(&array[i]);
+    allocator->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode), false);
+    new (array[i]) AllocTestLinkedListNode{nullptr, i, 0};
+    ASSERT_EQ(array[i]->next, nullptr);
+    ASSERT_EQ(array[i]->key, i);
+    ASSERT_EQ(array[i]->payload, 0);
+  }
+
+  pmwcas::NVRAM::Flush(sizeof(RootObj), root_obj);
+  pmwcas::NVRAM::Flush(sizeof(DescriptorPool), pool);
+  pmwcas::NVRAM::Flush(sizeof(nv_ptr<AllocTestLinkedListNode>) * kTestArraySize,
+                       array);
+  LOG(INFO) << "data flushed" << std::endl;
+
+  std::set<void *> user_allocations;
+  {
+    PMEMobjpool *pop = allocator->GetPool();
+    TOID(char) iter;
+    POBJ_FOREACH_TYPE(pop, iter) {
+      void *addr = pmemobj_direct(iter.oid);
+      if (!contains(base_allocations, addr)) {
+        insert(user_allocations, addr);
+      }
+    }
+  }
+
+  // We have 3 more allocations than just the nodes:
+  // * The DescriptorPool object
+  // * The actual descriptor pool (array)
+  // * The indirection array
+  ASSERT_EQ(user_allocations.size(), kTestArraySize + 3);
+
+  sem_post(sem);
+
+  std::vector<std::thread> threads;
+
+  Barrier barrier(kNumThreads);
+
+  for (uint32_t t = 0; t < kNumThreads; ++t) {
+    threads.emplace_back([&]() {
+      nv_ptr<AllocTestLinkedListNode> *addresses[kWordsToUpdate];
+      nv_ptr<AllocTestLinkedListNode> values[kWordsToUpdate];
+
+      RandomNumberGenerator rng(rand(), 0, kTestArraySize);
+      RandomNumberGenerator rng_op(rand(), 0, 100);
+      barrier.CountAndWait();
+
+      while (true) {
+        pool->GetEpoch()->Protect();
+
+        for (size_t cnt = 0; cnt < kOperationPerProtect; ++cnt) {
+          for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+            addresses[i] = nullptr;
+            values[i] = nullptr;
+          }
+
+          for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+          retry:
+            uint64_t idx = rng.Generate();
+            for (uint32_t j = 0; j < i; ++j) {
+              if (addresses[j] == &array[idx]) {
+                goto retry;
+              }
+            }
+
+            addresses[i] = &array[idx];
+            values[i] = reinterpret_cast<pmwcas::MwcTargetField<uint64_t> *>(
+                            &array[idx])
+                            ->GetValueProtected();
+          }
+
+          auto descriptor = pool->AllocateDescriptor();
+          EXPECT_NE(nullptr, descriptor.GetRaw());
+
+          for (uint32_t i = 0; i < kWordsToUpdate; ++i) {
+            if (rng_op.Generate() < kInsertPercentage) {
+              // Insert a new node
+              auto idx = descriptor.ReserveAndAddEntry(
+                  (uint64_t *)addresses[i], (uint64_t)values[i],
+                  Descriptor::kRecycleNewOnFailure);
+              uint64_t *ptr = descriptor.GetNewValuePtr(idx);
+              allocator->AllocateOffset(ptr, sizeof(AllocTestLinkedListNode));
+              AllocTestLinkedListNode *node =
+                  nv_ptr<AllocTestLinkedListNode>(descriptor.GetNewValue(idx));
+
+              new (node) AllocTestLinkedListNode{values[i], values[i]->key,
+                                                 values[i]->payload + 1};
+            } else {
+              // Remove an existing node
+              if (!values[i]->next) {
+                // Already the last node (sentinel); skip the deletion.
+                continue;
+              }
+              descriptor.AddEntry((uint64_t *)addresses[i], (uint64_t)values[i],
+                                  (uint64_t)values[i]->next,
+                                  Descriptor::kRecycleOldOnSuccess);
+            }
+          }
+
+          descriptor.MwCAS();
+        }
+
+        pool->GetEpoch()->Unprotect();
+      }
+    });
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+  LOG(INFO) << "Child process finished, this should not happen" << std::endl;
+}
+
+void ArrayPreScan(uint64_t *array) {
+  uint64_t dirty_cnt{0}, concas_cnt{0}, mwcas_cnt{0};
+  for (uint32_t i = 0; i < kTestArraySize; ++i) {
+    uint64_t val = array[i];
+    if ((val & pmwcas::Descriptor::kDirtyFlag) != 0) {
+      dirty_cnt += 1;
+      val = val & ~pmwcas::Descriptor::kDirtyFlag;
+    }
+    if ((val & pmwcas::Descriptor::kCondCASFlag) != 0) {
+      concas_cnt += 1;
+      val = val & ~pmwcas::Descriptor::kCondCASFlag;
+    }
+    if ((val & pmwcas::Descriptor::kMwCASFlag) != 0) {
+      mwcas_cnt += 1;
+      val = val & ~pmwcas::Descriptor::kMwCASFlag;
+    }
+  }
+  LOG(INFO) << "=======================\nDirty count: " << dirty_cnt
+            << "\tCondition CAS count: " << concas_cnt
+            << "\tMwCAS count: " << mwcas_cnt << std::endl;
+}
+
+std::set<void *> ArrayScan(nv_ptr<AllocTestLinkedListNode> *array) {
+  static const size_t kMaxLengthOutput = 10;
+
+  std::set<void *> allocations;
+
+  uint64_t length_cnt[kMaxLengthOutput + 1] = {};
+
+  auto getLength = [](nv_ptr<AllocTestLinkedListNode> head) {
+    return head ? std::min(head->payload + 1, kMaxLengthOutput) : 0;
+  };
+
+  for (uint32_t i = 0; i < kTestArraySize; ++i) {
+    nv_ptr<AllocTestLinkedListNode> head = array[i];
+
+    length_cnt[getLength(head)]++;
+
+    for (auto node = head; node; node = node->next) {
+      insert(allocations, (AllocTestLinkedListNode *)node);
+      if (node->next) {
+        EXPECT_EQ(node->key, node->key);
+        EXPECT_EQ(node->payload, node->next->payload + 1);
+      } else {
+        break;
+      }
+    }
+  }
+
+  EXPECT_EQ(length_cnt[0], 0);
+  LOG(INFO) << "=======================";
+  LOG(INFO) << "\tlength\tcount";
+  for (size_t i = 1; i <= kMaxLengthOutput; i++) {
+    LOG(INFO) << "\t" << i << "\t" << length_cnt[i];
+  }
+
+  return allocations;
+}
+
+GTEST_TEST(PMwCASMemorySafetyRecoveryTest, MultiThreadRecovery) {
+  auto allocator = reinterpret_cast<PMDKAllocator *>(Allocator::Get());
+
+  std::set<void *> base_allocations;
+  {
+    PMEMobjpool *pop = allocator->GetPool();
+    TOID(char) iter;
+    POBJ_FOREACH_TYPE(pop, iter) {
+      void *addr = pmemobj_direct(iter.oid);
+      insert(base_allocations, addr);
+    }
+  }
+
+  sem_t *sem = sem_open(kSemaphoreName, O_CREAT, 0666, 0);
+  if (sem == SEM_FAILED) {
+    LOG(FATAL) << "Semaphore creation failure, errno: " << errno;
+  }
+  sem_unlink(kSemaphoreName);
+  pid_t pid = fork();
+  if (pid == 0) {
+    child_process_work(sem, base_allocations);
+    return;
+  } else if (pid > 0) {
+    sem_wait(sem);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    kill(pid, SIGKILL);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  } else {
+    LOG(FATAL) << "fork failed" << std::endl;
+  }
+  LOG(INFO) << "Recovery starts here.";
+
+  auto root_obj = (RootObj *)allocator->GetRoot(sizeof(RootObj));
+  auto pool = (pmwcas::DescriptorPool *)pmemobj_direct(root_obj->pool);
+  auto array =
+      (nv_ptr<AllocTestLinkedListNode> *)pmemobj_direct(root_obj->array);
+
+  ArrayPreScan((uint64_t *)array);
+
+  pool->Recovery(false);
+
+  // We have 3 more allocations than just the nodes:
+  // * The DescriptorPool object
+  // * The actual descriptor pool (array)
+  // * The indirection array
+  std::set<void *> nodes = ArrayScan(array);
+  insert(nodes, pool);
+  insert(nodes, pool->GetDescriptor());
+  insert(nodes, array);
+
+  std::set<void *> user_allocations;
+  {
+    PMEMobjpool *pop = allocator->GetPool();
+    TOID(char) iter;
+    POBJ_FOREACH_TYPE(pop, iter) {
+      void *addr = pmemobj_direct(iter.oid);
+      if (!contains(base_allocations, addr)) {
+        insert(user_allocations, addr);
+      }
+    }
+  }
+
+  ASSERT_EQ(nodes.size(), user_allocations.size());
+  for (auto i = nodes.begin(), j = user_allocations.begin();
+       i != nodes.end() && j != user_allocations.end(); ++i, ++j) {
+    EXPECT_EQ(*i, *j);
+  }
+}
 #endif
 }  // namespace pmwcas
 
