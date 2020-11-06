356d59dd1f0df5a51f60c6886a6988bec1d93063
Ge Shi <utashih@gmail.com>
2020-11-05 21:10:58 -0800

    Refactor nv_ptr to make it more C++-ish

--- src/util/nv_ptr.h
+++ src/util/nv_ptr.h
@@ -2,51 +2,109 @@
 
 #include <include/pmwcas.h>
 
+#include <functional>
+#include <type_traits>
+
 namespace pmwcas {
 
 #ifdef PMEM
 template <typename T>
-struct nv_ptr {
-  nv_ptr() = default;
-  nv_ptr(std::nullptr_t) {}
-  nv_ptr(uint64_t offset) : offset_(offset) {}
-  nv_ptr(T *ptr) : offset_(fromPointer(ptr)) {}
-  T &operator*() { return *toPointer(offset_); }
-  T *operator->() { return toPointer(offset_); }
-  friend bool operator==(nv_ptr<T> l, nv_ptr<T> r) {
-    return l.offset_ == r.offset_;
-  }
-  friend bool operator!=(nv_ptr<T> l, nv_ptr<T> r) { return !(l == r); }
-  friend bool operator==(nv_ptr<T> l, std::nullptr_t) { return l.offset_ == 0; }
-  friend bool operator!=(nv_ptr<T> l, std::nullptr_t) { return l.offset_ != 0; }
-  friend bool operator==(nv_ptr<T> l, T *r) {
-    return toPointer(l.offset_) == r;
-  }
-  friend bool operator!=(nv_ptr<T> l, T *r) { return !(l == r); }
-  friend bool operator<(nv_ptr<T> l, nv_ptr<T> r) {
-    return l.offset_ < r.offset_;
-  }
+class nv_ptr {
+  // An nv_ptr acts like a regular pointer, except that it only
+  // keeps a 8-byte offset within a PM pool. The nv_ptr is swizzled
+  // at runtime with a call to the PM allocator.
+  using element_type = T;
+  using pointer = std::add_pointer_t<T>;
+  using reference = std::add_lvalue_reference_t<T>;
+  using offset_type = uint64_t;
 
-  operator uint64_t() const { return offset_; }
-  operator T *() const { return toPointer(offset_); }
+  offset_type offset_;
+
+ public:
+  constexpr nv_ptr() noexcept : offset_(0ull) {}
+  constexpr nv_ptr(std::nullptr_t) noexcept : offset_(0ull) {}
+  constexpr nv_ptr(offset_type offset) noexcept : offset_(offset) {}
+  constexpr nv_ptr(pointer ptr) noexcept : offset_(fromPointer(ptr)) {}
+
+  template <typename W, typename = std::enable_if_t<std::is_convertible_v<
+                            std::add_pointer_t<W>, pointer>>>
+  constexpr nv_ptr(nv_ptr<W> other) noexcept : offset_(other.get()) {}
+
+  constexpr pointer get() const noexcept { return toPointer(offset_); }
+
+  constexpr reference operator*() const { return *toPointer(offset_); }
+
+  constexpr pointer operator->() const noexcept { return toPointer(offset_); }
+
+  constexpr explicit operator bool() const noexcept { return offset_ != 0ull; }
+
+  constexpr explicit operator offset_type() const noexcept { return offset_; }
+
+  constexpr operator pointer() const noexcept { return toPointer(offset_); }
 
  private:
-  static uint64_t fromPointer(T *ptr) {
+  static offset_type fromPointer(pointer ptr) {
     auto allocator = reinterpret_cast<PMDKAllocator *>(Allocator::Get());
-    return allocator->GetOffset<T>(ptr);
+    return allocator->GetOffset<element_type>(ptr);
   }
 
-  static T *toPointer(uint64_t offset) {
+  static pointer toPointer(offset_type offset) {
     auto allocator = reinterpret_cast<PMDKAllocator *>(Allocator::Get());
-    return allocator->GetDirect<T>(offset);
+    return allocator->GetDirect<element_type>(offset);
   }
-
-  template <typename U>
-  friend nv_ptr<U> CompareExchange64(nv_ptr<U> *destination,
-                                     nv_ptr<U> new_value, nv_ptr<U> comparand);
-
-  uint64_t offset_{0};
 };
+
+template <typename T1, typename T2>
+bool operator==(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return a.get() == b.get();
+}
+
+template <typename T1, typename T2>
+bool operator!=(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return !(a == b);
+}
+
+template <typename T>
+bool operator==(nv_ptr<T> p, std::nullptr_t) {
+  return !p;
+}
+
+template <typename T>
+bool operator==(std::nullptr_t, nv_ptr<T> p) {
+  return !p;
+}
+
+template <typename T>
+bool operator!=(nv_ptr<T> p, std::nullptr_t) {
+  return (bool)p;
+}
+
+template <typename T>
+bool operator!=(std::nullptr_t, nv_ptr<T> p) {
+  return (bool)p;
+}
+
+template <typename T1, typename T2>
+bool operator<(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return std::less<
+      std::common_type_t<std::add_pointer_t<T1>, std::add_pointer_t<T2>>>()(
+      a.get(), b.get());
+}
+
+template <typename T1, typename T2>
+bool operator>(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return b < a;
+}
+
+template <typename T1, typename T2>
+bool operator<=(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return !(a > b);
+}
+
+template <typename T1, typename T2>
+bool operator>=(nv_ptr<T1> a, nv_ptr<T2> b) {
+  return !(a < b);
+}
 
 template <typename T>
 nv_ptr<T> CompareExchange64(nv_ptr<T> *destination, nv_ptr<T> new_value,
@@ -65,3 +123,14 @@ using nv_ptr = T *;
 
 #endif
 }  // namespace pmwcas
+
+#ifdef PMEM
+namespace std {
+template <typename T>
+struct hash<pmwcas::nv_ptr<T>> {
+  size_t operator()(const pmwcas::nv_ptr<T> &ptr) const noexcept {
+    return hash<add_pointer_t<T>>()(ptr.get());
+  }
+};
+}  // namespace std
+#endif
